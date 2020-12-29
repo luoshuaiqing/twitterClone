394942f604a74059052acf815c1afcf28b15e787
Shuaiqing Luo <shuaiqin@usc.edu>
2020-12-29 14:31:41 -0800

    update

--- TwitterClone/API/TweetService.swift
+++ TwitterClone/API/TweetService.swift
@@ -10,7 +10,7 @@ import Firebase
 struct TweetService {
     static let shared = TweetService()
     
-    func uploadTweet(caption: String, completion: @escaping(Error?, DatabaseReference) -> Void) {
+    func uploadTweet(caption: String, type: UploadTweetConfiguration, completion: @escaping(Error?, DatabaseReference) -> Void) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         let values = [
@@ -21,11 +21,14 @@ struct TweetService {
             "caption": caption
         ] as [String: Any]
         
-        let ref = REF_TWEETS.childByAutoId()
-        
-        ref.updateChildValues(values) { (err, ref) in
-            guard let tweetID = ref.key else { return }
-            REF_USER_TWEETS.child(uid).updateChildValues([tweetID: 1], withCompletionBlock: completion)
+        switch type {
+        case .tweet:
+            REF_TWEETS.childByAutoId().updateChildValues(values) { (err, ref) in
+                guard let tweetID = ref.key else { return }
+                REF_USER_TWEETS.child(uid).updateChildValues([tweetID: 1], withCompletionBlock: completion)
+            }
+        case .reply(let tweet):
+            REF_TWEET_REPLIES.child(tweet.tweetId).childByAutoId().updateChildValues(values, withCompletionBlock: completion)
         }
     }
     
