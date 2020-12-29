99ade88b92189a4a5fde904c17a6580411cf6a3b
Shuaiqing Luo <shuaiqin@usc.edu>
2020-12-29 13:21:41 -0800

    update

--- TwitterClone/Controller/UploadTweetController.swift
+++ TwitterClone/Controller/UploadTweetController.swift
@@ -12,6 +12,8 @@ class UploadTweetController: UIViewController {
     // MARK: - Properties
     
     private let user: User
+    private let config: UploadTweetConfiguration
+    private lazy var viewModel = UploadTweetViewModel(config: config)
     
     //we need to declare this as a lazy var, because we want to access "self"
     private lazy var actionButton: UIButton = {
@@ -44,8 +46,9 @@ class UploadTweetController: UIViewController {
     
     // MARK: - Lifecycle
     
-    init(user: User) {
+    init(user: User, config: UploadTweetConfiguration) {
         self.user = user
+        self.config = config
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -56,6 +59,13 @@ class UploadTweetController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        
+        switch config {
+        case .tweet:
+            print("DEBUG: Config is tweet")
+        case .reply(let tweet):
+            print("DEBUG: Replying to \(tweet.caption)")
+        }
     }
     
     // MARK: Selectors
---
+++ TwitterClone/ViewModel/UploadTweetViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  UploadTweetViewModel.swift
+//  TwitterClone
+//
+//  Created by 罗帅卿 on 12/29/20.
+//
+
+import UIKit
+
+enum UploadTweetConfiguration {
+    case tweet
+    case reply(Tweet)
+}
+
+struct UploadTweetViewModel {
+    
+    let actionButtonTitle: String
+    let placeholderText: String
+    let shouldShowReplyLabel: Bool
+    var replyText: String?
+    
+    init(config: UploadTweetConfiguration) {
+        switch config {
+        case .tweet:
+            actionButtonTitle = "Tweet"
+            placeholderText = "What's happening"
+            shouldShowReplyLabel = false
+        case .reply(let tweet):
+            actionButtonTitle = "Reply"
+            placeholderText = "Tweet your reply"
+            shouldShowReplyLabel = true
+            replyText = "Replying to @\(tweet.user.userName)"
+        }
+    }
+}
