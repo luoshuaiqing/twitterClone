dc961d4540fa62eb03d201681b5f166f12a5f485
Shuaiqing Luo <shuaiqin@usc.edu>
2020-12-31 17:35:13 -0800

    update

---
+++ TwitterClone/Model/Notification.swift
@@ -0,0 +1,39 @@
+//
+//  Notification.swift
+//  TwitterClone
+//
+//  Created by 罗帅卿 on 12/31/20.
+//
+
+import Foundation
+
+enum NotificationType: Int {
+    case follow
+    case like
+    case reply
+    case retweet
+    case mention
+}
+
+struct Notification {
+    let tweetID: String?
+    var timestamp: Date!
+    let user: User
+    var tweet: Tweet?
+    var type: NotificationType!
+    
+    init(user: User, tweet: Tweet?, dict: [String: AnyObject]) {
+        self.user = user
+        self.tweet = tweet
+        
+        self.tweetID = dict["tweetID"] as? String ?? ""
+        
+        if let timestamp = dict["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        if let type = dict["type"] as? Int {
+            self.type = NotificationType(rawValue: type)
+        }
+    }
+}
